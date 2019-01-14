15ed5ff93cc8b11043b90c209bf7cb546fc25a29
monalisaoverdrive88 <joekreepto@joekreepto.station>
2019-01-14 12:43:16 +0100

    Text

---
+++ resources/views/blockchain.php
@@ -0,0 +1,204 @@
+<?php echo $header; ?>
+<main class="position-relative overflow-hidden">
+		<!-- ./Page header -->
+		<header class="page header color-1 overlay alpha-8 image-background cover gradient gradient-43">
+			<div class="divider-shape">
+				<!-- waves divider -->
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none" class="shape-waves" style="left: 0; transform: rotate3d(0,1,0,180deg);">
+					<path class="shape-fill shape-fill-1" d="M790.5,93.1c-59.3-5.3-116.8-18-192.6-50c-29.6-12.7-76.9-31-100.5-35.9c-23.6-4.9-52.6-7.8-75.5-5.3c-10.2,1.1-22.6,1.4-50.1,7.4c-27.2,6.3-58.2,16.6-79.4,24.7c-41.3,15.9-94.9,21.9-134,22.6C72,58.2,0,25.8,0,25.8V100h1000V65.3c0,0-51.5,19.4-106.2,25.7C839.5,97,814.1,95.2,790.5,93.1z" />
+				</svg>
+			</div>
+			<div class="container" style="">
+				<div class="row">
+					<div class="col-md-6">
+						<h1 class="regular display-4 color-1 mb-4">La Blockchain di Scrypta</h1>
+						<p class="lead color-1">Scrypta è una piattaforma distribuita che ha in sé il potenziale per ridefinire i modelli di business e le relazioni tra cittadini, imprese e governi, rendendo i dati unici, immutabili, tracciabili e non falsificabili.</p>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- ./Overview - Floating boxes -->
+		<section class="section overview">
+			<div class="container">
+				<div class="row align-items-center gap-y">
+					<div class="col-lg-5 mr-auto text-center text-md-left">
+						<div class="section-heading">
+							<p class="badge badge-success bold">Blockchain Technology</p>
+							<h2>Distributed Ledger and P2P Network</h2>
+							<p class="lead color-2">La blockchain di Scrypta è composta da due elementi chiave: il database distribuito e una rete decentralizzata.
+</p>
+						</div>
+						<p>Queste due entità costituiscono il “distributed ledger” sul quale si registrano informazioni e valori. Tutti coloro che fanno parte del network di Scrypta hanno una copia identica del registro, e qualsiasi cambiamento viene apportato si riflette immediatamente in tutte le copie disponibili.</p>
+					</div>
+					<div class="col-lg-6">
+						<div class="row gap-y">
+							<div class="col-6 col-sm-5 col-md-6 mt-6 ml-lg-auto">
+								<div class="card rounded p-2 p-sm-4 shadow text-center text-md-left" data-aos="fade-up">
+									<i class="pe pe-3x pe-7s-settings color-3"></i>
+									<p class="bold mb-0">Nodi</p>
+									<p class="op-7 small">Rappresentano i membri della Blockchain e sono costituiti dai computer di ciascun utente connessi alla rete di Scrypta.
+</p>
+								</div>
+							</div>
+							<div class="col-6 col-sm-5 col-md-6 mr-lg-auto">
+								<div class="color-1 bg-4-gradient card rounded p-2 p-sm-4 shadow text-center text-md-left" data-aos="fade-up">
+									<i class="pe pe-3x pe-7s-wallet"></i>
+									<p class="bold mb-0">Transazioni</p>
+									<p class="op-7 small">Costituite dai dati che rappresentano i valori oggetto di “scambio” e che necessitano di essere verificati, approvati e archiviati.
+</p>
+								</div>
+							</div>
+							<div class="col-6 col-sm-5 col-md-6 ml-lg-auto">
+								<div class="card rounded p-2 p-sm-4 shadow text-center text-md-left" data-aos="fade-up">
+									<i class="pe pe-3x pe-7s-plugin color-3"></i>
+									<p class="bold mb-0">Blocchi</p>
+									<p class="op-7 small">Creati ogni 60 secondi dall’algoritmo che regola Scrypta, formano il raggruppamento di un insieme di transazioni,  unite per formare i diversi anelli della catena.</p>
+								</div>
+							</div>
+							<div class="col-6 col-sm-5 col-md-6 mt-6n mr-lg-auto">
+								<div class="card rounded p-2 p-sm-4 shadow text-center text-md-left" data-aos="fade-up">
+									<i class="pe pe-3x pe-7s-note2 color-3"></i>
+									<p class="bold mb-0">Registro Pubblico</p>
+									<p class="op-7 small">E’ il “libro mastro” nel quale vengono “scritte” con la massima trasparenza e in modo immutabile tutte le transazioni effettuate, in modo ordinato e sequenziale.
+</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- ./Dashboard Included -->
+		<section class="section bg-6 top-left">
+			<div class="container bring-to-front">
+				<div class="section-heading text-center">
+					<h2>Features</h2>
+					<p class="lead">L’algoritmo Quark di Scrypta prevede la generazione di nuove Lyra ad ogni blocco. Queste vengono assegnate come ricompense ai membri del Network attraverso un protocollo di consenso di tipo Masternode/PoS (Proof of Stake).</p>
+				</div>
+				<div class="row align-items-center gap-y">
+					<div class="col-md-12 text-center text-md-left">
+						<ul class="list-unstyled">
+							<li class="media d-block d-lg-flex">
+								<i class="pe pe-3x pe-7s-share accent icon-lg mr-3 text-lg-right"></i>
+								<div class="media-body mt-3 mt-lg-0 text-center text-md-left">
+									<h5 class="bold color-5">Masternode</h5>
+									<p class="mt-0 mb-5">Un masternode è un server, online 24 ore su 24, 7 giorni su 7, che opera come super validatore. Per incentivare il loro funzionamento, i masternode ricevono ricompense in LYRA. Questo può essere inteso come un reddito passivo calcolabile e particolarmente prevedibile. Operare come validatore masternode richiede la proprietà di 15000 LYRA.
+</p>
+								</div>
+							</li>
+							<li class="media d-block d-lg-flex">
+								<i class="pe pe-3x pe-7s-keypad accent icon-lg mr-3 text-lg-right"></i>
+								<div class="media-body mt-3 mt-lg-0 text-center text-md-left">
+									<h5 class="bold color-5">Staking</h5>
+									<p class="mt-0 mb-5">Con il Proof of Stake, il creatore di un nuovo blocco viene scelto in modo deterministico a seconda della sua ‘ricchezza’, definita come ‘stake’. L'algoritmo di consenso di Scrypta seleziona un validatore ogni 60 secondi, gli assegna le spettanti ricompense e gli conferisce il diritto di creare un singolo blocco.  In altre parole, significa che i “portafogli digitali” aperti alla rete, coniano nuove monete generando un reddito passivo.</p>
+								</div>
+							</li>
+							<li class="media d-block d-lg-flex">
+								<i class="pe pe-3x pe-7s-refresh-2 accent icon-lg mr-3 text-lg-right"></i>
+								<div class="media-body mt-3 mt-lg-0 text-center text-md-left">
+									<h5 class="bold color-5">See-Saw Mechanism</h5>
+									<p class="mt-0">Al fine di assegnare la distribuzione dei premi in modo efficiente, Scrypta adotta un sistema che modifica in modo dinamico il rapporto tra ricompense per i masternode e ricompense per lo staking. Il meccanismo See-Saw consente così a tutti i possessori di Lyra di essere comunque premiati per detenere del valore e svolgere la funzione di “nodo”, ottenendo così un sistema più equo e meno centralizzato.</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- ./Features - Boxed -->
+		<section class="bg-1 top-left">
+			<div class="container">
+				<div class="section-heading text-center">
+					<h2>Specifiche Tecniche</h2>
+					<p class="lead color-2">-</p>
+				</div>
+				<div class="row gap-y">
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							    <p class="mb-0">TICKER</p>
+								<h3 class="bold">LYRA</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							     <p class="mb-0">ALGORITMO</p>
+								<h3 class="bold">QUARK</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							<p class="mb-0">COIN TAPE</p>
+								<h3 class="bold">MN/PoS</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							    <p class="mb-0">60 SEC.</p>
+								<h3 class="bold">60 sec.</h2>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							    <p class="mb-0">MAX SUPPLY</p>
+								<h3 class="bold">50M LYRA</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							<p class="mb-0">RPC/P2P PORTS</p>
+								<h3 class="bold">42223 / 42222</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+							<p class="mb-0">MASTERNODE COLLATERAL</p>
+								<h3 class="bold">15.000 LYRA</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="shadow-box shadow-hover border-0 card">
+							<div class="card-body text-center">
+								<p class="mb-0">MINIMUM STAKE AGE</p>
+								<h3 class="bold">60 min.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- ./Customers -->
+		<section class="section bg-1 edge bottom-right">
+			<div class="container">
+				<div class="section-heading text-center">
+					<h2>Scrypta Block Explorer</h2>
+					<p class="color-2 lead">Lo strumento chiave per esplorare i blocchi e la loro estrazione, ottenere informazioni su indirizzi e transazioni e conoscere lo stato della Blockchain di Scrypta.</p>
+				</div>
+				<div class="text-center mt-2">
+					<a href="https://chainz.cryptoid.info/lyra/" target="_blank" class="btn btn-rounded btn-accent">Entra</a>
+				</div>
+			</div>
+		</section>
+		<!-- ./Team -->
+	</main>
+<?php echo $footer; ?>
