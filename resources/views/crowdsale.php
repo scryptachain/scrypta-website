23b36f04cc0129c6abcde313a15009089f613528
Mona Lisa <joekreepto@gmail.com>
2019-02-15 20:03:51 +0100

    Crowdsale edit 2

--- resources/views/crowdsale.php
+++ resources/views/crowdsale.php
@@ -2,7 +2,7 @@
 
 <main>
 	<!-- ./Page header -->
-	<header class="page header color-1 overlay alpha-8 image-background cover overlay gradient gradient-43" style="background-image: url('img/bg/waves.jpg')">
+	<header class="page header color-1 overlay alpha-8 image-background cover overlay gradient gradient-43" style="">
 		<div class="divider-shape">
 			<!-- waves divider -->
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none" class="shape-waves" style="left: 0; transform: rotate3d(0,1,0,180deg);">
@@ -13,7 +13,7 @@
 			<div class="row">
 				<div class="col-md-6">
 					<h1 class="regular display-4 color-1 mb-4">Scrypta Crowdsale</h1>
-					<p class="lead color-1">Distribution stage that will allow participants to contribute to the project by collaborating on the growth of the infrastructure.</p>
+					<p class="lead color-1">Welcome to the official page of the Scrypta Crowdsale. During this distribution stage all participants can contribute to the project by collaborating on the growth of the infrastructure.</p>
 				</div>
 			</div>
 		</div>
@@ -23,7 +23,7 @@
 		<div class="container">
 			<div class="section-heading text-center">
 				<h2 class="mb-2">How to Contribute</h2>
-				<p class="lead color-2">Scrypta will distribute the crowdsale LYRA coins batch to cover the initial costs for the development and deployment of the infrastructure.</p>
+				<p class="lead color-2">In order to join the Crowdsale, all partecipants can proceed through methods described below.</p>
 			</div>
 			<div class="row">
 			<div class="col-md-6 mt-5">
@@ -39,20 +39,20 @@
 							<li class="list-group-item ">15000 LYRA</li>
 							<li class="list-group-item ">1 Year Masternode Hosting</li>							
 							<li class="list-group-item">Automatic Masternode Deployment</li>
-							<li class="list-group-item">Bonus 7500Lyra after 1yr*</li>
+							<li class="list-group-item">Bonus 7500 Lyra *</li>
 							<li class="list-group-item">Staking in cloud</li>
 						</ul>
 						<div class="card-body">
-							<a href="#" class="btn btn-accent">Create Now</a>
+							<a href="#" class="btn btn-accent">Join Crowdsale</a>
 						</div>
 					</div>
-					* after 1 year of continued masternode activity without interruption.
+					* The Bonus will be issued after 1 year of continued masternode activity without interruption.
 				</div>
 				<div class="col-md-6 mt-5">
 					<div class="card text-center">
-						<div class="pricing card-header bg-3-gradient color-1 d-flex align-items-center flex-column">
-							<h4>Direct Listing</h4>
-							<div style="font-size:35px; margin:24px 0">
+						<div class="pricing card-header bg-2-gradient color-1 d-flex align-items-center flex-column">
+							<h4>Direct Exchange Listing</h4>
+							<div style="font-size:35px; margin:13px 0">
 								<span class="price bold">Mercatox</span>
 							</div>
 							<p>Gradual placing of LYRA into “direct listing” on the Mercatox.com exchange.</p>
@@ -60,133 +60,19 @@
 						<ul class="list-group list-group-flush">
 							<li class="list-group-item">No predetermined amount</li>
 							<li class="list-group-item strike-through">1 year Masternode Hosting</li>
+							<li class="list-group-item strike-through">Automatic Masternode Deployment</li>
 							<li class="list-group-item strike-through">Bonus 50% MN Locked 1yr</li>
 							<li class="list-group-item strike-through">Staking in Cloud</li>
 						</ul>
 						<div class="card-body">
-							<a href="#" class="btn btn-accent">Buy now</a>
+							<a href="https://mercatox.com/exchange/LYRA/BTC" target="_blank" class="btn btn-accent">Trade now</a>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-	<!-- ./Plans features -->
-	<section>
-		<div class="container">
-			<div class="section-heading text-center">
-				<h2>All our plans include</h2>
-				<p class="lead color-2">Get access to a ton of features out of the box</p>
-			</div>
-			<div class="row gap-y text-center text-md-left">
-				<div class="col-md-4">
-					<i class="pe pe-3x color-4 pe-7s-headphones icon m-0"></i>
-					<h5 class="bold my-3">First class support</h5>
-					<p class="my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-				</div>
-				<div class="col-md-4">
-					<i class="pe pe-3x color-4 pe-7s-plugin icon m-0"></i>
-					<h5 class="bold my-3">Code snippets</h5>
-					<p class="my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-				</div>
-				<div class="col-md-4">
-					<i class="pe pe-3x color-4 pe-7s-headphones icon m-0"></i>
-					<h5 class="bold my-3">Full documentation</h5>
-					<p class="my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-				</div>
-				<div class="col-md-4">
-					<i class="pe pe-3x color-4 pe-7s-door-lock icon m-0"></i>
-					<h5 class="bold my-3">Total control of your code</h5>
-					<p class="my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-				</div>
-				<div class="col-md-4">
-					<i class="pe pe-3x color-4 pe-7s-airplay icon m-0"></i>
-					<h5 class="bold my-3">Responsive design</h5>
-					<p class="my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-				</div>
-				<div class="col-md-4">
-					<i class="pe pe-3x color-4 pe-7s-browser icon m-0"></i>
-					<h5 class="bold my-3">Browser support</h5>
-					<p class="my-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae dicta dolores hic porro quam voluptatibus.</p>
-				</div>
-			</div>
-		</div>
-	</section>
-	</section>
-	<!-- ./Register -->
-	<section class="section">
-		<div class="container bring-to-front">
-			<div class="shadow rounded text-center bg-4-gradient color-1 p-5">
-				<h2 class="mb-5">Mercatox Exchange Direct Listing</h2>
-				<p class="handwritten highlight font-md">Why wait? Start now!</p>
-				<a href="signup.html" class="btn btn-5 btn-rounded mt-4">LYRA/BTC</a>
-				<a href="signup.html" style="margin-left:15px" class="btn btn-5 btn-rounded mt-4">LYRA/USDC</a>
-			</div>
-		</div>
-	</section>
-	<!-- ./FAQs -->
-	<section class="section bg-6 edge bottom-right">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<h2>Do you have
-						<span class="bold">questions</span>
-					</h2>
-					<p class="lead">Not sure how this template can help you? Wonder why you need to buy our theme?</p>
-					<p class="text-muted">Here are the answers to some of the most common questions we hear from our appreciated customers</p>
-				</div>
-				<div class="col-md-8">
-					<div class="accordion accordion-clean" id="common-faqs-accordion">
-						<div class="card mb-3">
-							<div class="card-header">
-								<a href="#" class="card-title btn" data-toggle="collapse" data-target="#q1">What does the package include?</a>
-							</div>
-							<div id="q1" class="collapse show" data-parent="#common-faqs-accordion">
-								<div class="card-body">When you buy Dashcore, you get all you see in the demo but the images. We include SASS files for styling, complete JS files with comments, all HTML variations. Besides we include all mobile PSD mockups.</div>
-							</div>
-						</div>
-						<div class="card mb-3">
-							<div class="card-header">
-								<a href="#" class="card-title btn" data-toggle="collapse" data-target="#q2">What is the typical response time for a support question?</a>
-							</div>
-							<div id="q2" class="collapse" data-parent="#common-faqs-accordion">
-								<div class="card-body">Since you report us a support question we try to make our best to find out what is going on, depending on the case it could take more or les time, however a standard time could be minutes or hours.</div>
-							</div>
-						</div>
-						<div class="card mb-3">
-							<div class="card-header">
-								<a href="#" class="card-title btn" data-toggle="collapse" data-target="#q3">What do I need to know to customize this template?</a>
-							</div>
-							<div id="q3" class="collapse" data-parent="#common-faqs-accordion">
-								<div class="card-body">Our documentation give you all you need to customize your copy. However you will need some basic web design knowledge (HTML, Javascript and CSS)</div>
-							</div>
-						</div>
-						<div class="card">
-							<div class="card-header">
-								<a href="#" class="card-title btn" data-toggle="collapse" data-target="#q4">Can I suggest a new feature?</a>
-							</div>
-							<div id="q4" class="collapse" data-parent="#common-faqs-accordion">
-								<div class="card-body">Definitely
-									<span class="bold">Yes</span>, you can contact us to let us know your needs. If your suggestion represents any value to both we can include it as a part of the theme or you can request a custom build by an extra cost. Please note it could take some time in order for the feature to be implemented.</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 	
-	<!-- ./CTA - Create Account -->
-	<section class="section bg-1 edge top-left b-b">
-		<div class="container pt-5">
-			<div class="d-flex align-items-center flex-column flex-md-row">
-				<div class="text-center text-md-left">
-					<p class="light mb-0 accent lead">Ready to get started?</p>
-					<h2 class="mt-0">Create a masternode now</h2>
-				</div>
-				<a href="register.html" class="btn btn-accent btn-rounded mt-3 mt-md-0 ml-md-auto">Scrypta Masternodes Platform</a>
-			</div>
-		</div>
-	</section>
 </main>
 
 <?php echo $footer; ?>
