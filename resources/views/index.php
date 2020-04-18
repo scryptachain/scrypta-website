e40ccf52f1059bcc0938adcfeee6fe72fb0d86f8
Steve-Rog <steve-rog@gmail.com>
2020-04-18 13:58:54 +0200

    update

--- resources/views/index.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <?php echo $header; ?>
-    <div class="text-center" style="padding-top:15%">
-	  <img src="/logo_full.png" width="150"><br><br>
-      Sistemi decentralizzati<br>di archiviazione e verifica
-      <br><br>Presto online.
-    </div>
-</html>
